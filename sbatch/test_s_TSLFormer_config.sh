4492c9a88bb786ce7873d668cb75b0eafbe314b4
hatch5o6 <hatch5o6@byu.edu>
2026-01-12 14:15:12 -0700

    update

--- sbatch/test_s_TSLFormer_config.sh
+++ sbatch/test_s_TSLFormer_config.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #SBATCH --time=72:00:00   # walltime.  hours:minutes:seconds
-#SBATCH --ntasks-per-node=4
+#SBATCH --ntasks-per-node=1
 #SBATCH --nodes=1
 #SBATCH --mem=1024000M
-#SBATCH --gpus=4
+#SBATCH --gpus=1
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
